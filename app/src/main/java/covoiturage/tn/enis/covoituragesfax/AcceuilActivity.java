48f8fcf7a2ac6925eaab66f4806cf02ab21677ef
khouloudbelguith <khouloud.belguiith@gmail.com>
2016-11-13 19:06:10 +0100

    ajouter des methodes onClick()

--- app/src/main/java/covoiturage/tn/enis/covoituragesfax/AcceuilActivity.java
+++ app/src/main/java/covoiturage/tn/enis/covoituragesfax/AcceuilActivity.java
@@ -4,7 +4,10 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -12,9 +15,11 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class AcceuilActivity extends AppCompatActivity {
+    private static final String TAG = "EmailPassword";
     private EditText mEmailField;
     private EditText mPasswordField;
     private FirebaseAuth firebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +32,14 @@ public class AcceuilActivity extends AppCompatActivity {
         firebaseAuth= FirebaseAuth.getInstance();
 
     }
+
+
     //validate email & password
     private boolean validateForm() {
+        String email = mEmailField.getText().toString();
         boolean valid = true;
 
-        String email = mEmailField.getText().toString();
+
         if (TextUtils.isEmpty(email)) {
             mEmailField.setError("Required.");
             valid = false;
@@ -49,20 +57,70 @@ public class AcceuilActivity extends AppCompatActivity {
 
         return valid;
     }
-    private void registerUser(){
+
+    private void registerUser(String email,String password){
         //creating a new user
-        firebaseAuth.createUserWithEmailAndPassword("","")
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         //check if success
                         if (task.isSuccessful())
-                        {//message
+                        {
+                            Toast.makeText(AcceuilActivity.this, "user registred", Toast.LENGTH_SHORT).show();
                         }
                         else {
-                            //message
+                            Toast.makeText(AcceuilActivity.this, "echec", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
+    }
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        //showProgressDialog();
+
+        // [START sign_in_with_email]
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                       /* if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            Toast.makeText(EmailPasswordActivity.this, R.string.auth_failed,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // [START_EXCLUDE]
+                        if (!task.isSuccessful()) {
+                            mStatusTextView.setText(R.string.auth_failed);
+                        }
+                        hideProgressDialog();*/
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
+    private void signOut() {
+        firebaseAuth.signOut();
+    }
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.email_create_account_button) {
+            registerUser(mEmailField.getText().toString(), mPasswordField.getText().toString());
+        } else if (i == R.id.email_sign_in_button) {
+            signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
+        } else if (i == R.id.sign_out_button) {
+            signOut();
+        }
     }
 }
